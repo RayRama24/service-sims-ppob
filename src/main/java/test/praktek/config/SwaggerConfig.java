9ac037bbdadd125e65e98ea9297a7ef783d4fb3f
raihan ramadhan <raihanramadhann24@gmail.com>
2025-12-05 15:21:10 +0700

    add config swager

---
+++ src/main/java/test/praktek/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package test.praktek.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://service-sims-ppob-production.up.railway.app"));
+    }
+}
